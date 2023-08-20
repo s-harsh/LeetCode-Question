72f93f01d99d27e19445d255664104d970e2262a
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-21 03:23:24 +0530

    Create 904. Fruit Into Baskets
    Basically the problem is to find largest substring of k==2 distinct elements

---
+++ 904. Fruit Into Baskets.cpp
@@ -0,0 +1,25 @@
+// Variable sized sliding window problem
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        if(fruits.size()==1){
+            return 1;
+        }
+        int i=0,j=0,n=fruits.size(),ans=0;
+        unordered_map<int,int>mp;
+        while(j<n){
+            mp[fruits[j]]++;
+            while(mp.size()>2){
+                mp[fruits[i]]--;
+                if(mp[fruits[i]]==0){
+                    mp.erase(fruits[i]);
+                }
+                i++;
+            }
+            ans=max(ans,(j-i+1));
+            j++;
+        }
+        return ans;
+    }
+};
