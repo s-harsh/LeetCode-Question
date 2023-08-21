0a1a5d7f0f7cdfe10e21da6e51aa46586da91f1e
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-21 22:58:47 +0530

    Create Sort an Array using recursion

---
+++ Sort an Array using recursion.cpp
@@ -0,0 +1,27 @@
+// TLE
+class Solution {
+public:
+    vector<int> insert(vector<int>& v, int temp) {
+        int len = v.size();
+        if (len == 0 || v[len - 1] <= temp) {
+            v.push_back(temp);
+            return v;
+        }
+        int val = v[len - 1];
+        v.pop_back();
+        insert(v, temp);
+        v.push_back(val);
+        return v;
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums;
+        }
+        int temp = nums[nums.size() - 1];
+        nums.pop_back();
+        sortArray(nums);
+        insert(nums, temp);
+        return nums;
+    }
+};
